a28ca6d3c6a102f4d4428e97fd01a3cb2e0e25b0
jrasero <jrasero.daparte@gmail.com>
2021-03-08 11:46:02 -0500

    update comments

--- my_sklearn_tools/model_selection.py
+++ my_sklearn_tools/model_selection.py
@@ -22,24 +22,27 @@ class StratifiedKFoldReg(StratifiedKFold):
         # into bins of quasi equal size
         n_labels = int(np.round(n_samples/self.n_splits))
         
+        # Assign a label to each bin of n_splits points
+        y_labels_sorted = np.concatenate([np.repeat(ii, self.n_splits) \
+            for ii in range(n_labels)])
+        
         # Get number of points that would fall
         # out of the equally-sized bins
         mod = np.mod(n_samples, self.n_splits)
         
-        y_labels_sorted = np.concatenate([np.repeat(ii, self.n_splits) \
-            for ii in range(n_labels)])
-        
         # Find unique idxs of first unique label's ocurrence
         _, labels_idx = np.unique(y_labels_sorted, return_index=True)
         
-        # sample randomly the label idxs to which the assign the 
+        # sample randomly the label idxs to which assign the 
         # the mod points
         rand_label_ix = np.random.choice(labels_idx, mod, replace=False)
 
-        # insert these before 
-        y_labels_sorted = np.insert(y_labels_sorted, rand_label_ix, y_labels_sorted[rand_label_ix])
+        # insert these at the beginning of the corresponding bin
+        y_labels_sorted = np.insert(y_labels_sorted, 
+                                    rand_label_ix, 
+                                    y_labels_sorted[rand_label_ix])
         
-        # find each element of y which label corresponds in the sorted 
+        # find each element of y to which label corresponds in the sorted 
         # array of labels
         map_labels_y = dict()
         for ix, label in zip(np.argsort(y), y_labels_sorted):
